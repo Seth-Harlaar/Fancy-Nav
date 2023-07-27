c04bd78e5d65697c0ff81568681462c080bd2b2f
Seth-Harlaar <92818054+Seth-Harlaar@users.noreply.github.com>
2023-07-27 08:40:23 -0400

    update homepage with more info

---
+++ src/pages/hero/heroElements.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+
+export const Title = styled.h1`
+  text-transform: uppercase;
+`
+
+export const SmallText = styled.div`
+  font-size: 20px;
+  line-height: 2;
+`
+
+export const Link = styled.a`
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+export const HighlightText = styled.span`
+  color: darkcyan;
+`
--- src/pages/hero/index.js
+++ src/pages/hero/index.js
@@ -1,13 +1,22 @@
-
-
+import {SmallText, Link, HighlightText, Title} from './heroElements';
 
 
 const HeroPage = function(){
 
   return(
     <>
-      Home Page - Hello World
-
+      <Title>Welcome to my <HighlightText>Fancy Nav app</HighlightText></Title>
+      <SmallText>
+        This website was built to practice my web development skills.
+        <br/>
+        This website is built only using <HighlightText>React.js, styled-components, and CSS</HighlightText> .
+        <br/>
+        Credit to <HighlightText>Bucky Maler + Sergey Melnik</HighlightText> for the navigation design, I really liked their work and wanted to repoduce it.
+        <br/>
+        You can find the original here: <HighlightText><Link href="https://www.free-css.com/free-css-templates/page288/global">https://www.free-css.com/free-css-templates/page288/global</Link></HighlightText>.
+        <br/>
+        Make sure to click the <HighlightText>hamburger menu</HighlightText> in the top right to see the navigation design.
+      </SmallText>
     </>
   )
 }
